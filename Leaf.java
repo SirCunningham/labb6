d82b985b4f71207e03e7e76d3a3eafe9b513c336
BanachObama <BanachObama@gmail.com>
2013-12-22 12:00:34 +0100

    Create Leaf.java

---
+++ Leaf.java
@@ -0,0 +1,29 @@
+package labb6;
+
+public class Leaf implements Component {
+
+    String name;
+    double weight;
+
+    public Leaf(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public void add(Component component) {
+    }
+
+    public void remove(Component component) {
+    }
+
+    public Component getChild(int level) {
+        return null;
+    }
+
+    public String toString() {
+        return "en " + name + " ";
+    }
+    public double getWeight() {
+        return weight;
+    }
+}
