2b3af76139f30dd5d3eadf1b9c19cb54150a30b3
SirCunningham <lvckv_v@yahoo.se>
2014-02-14 17:54:31 +0100

    Update BuilderList.java

--- BuilderList.java
+++ BuilderList.java
@@ -1,5 +1,6 @@
 package labb6;
 
+\\Create both lists, use when appropriate, and create a builder between them.
 public class BuilderList implements List {
 
     public static void main(String[] args) {
