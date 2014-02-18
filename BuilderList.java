61c2ef8a3afb30f53d3d94c2fd49d49eb1f1c521
Anon <anon@example.com>
2014-02-18 17:07:57 +0100

    .

--- BuilderList.java
+++ BuilderList.java
@@ -1,6 +1,8 @@
 package labb6;
 
-\\Create both lists, use when appropriate, and create a builder between them.
+import java.util.List;
+
+//Create both lists, use when appropriate, and create a builder between them.
 public class BuilderList implements List {
 
     public static void main(String[] args) {
