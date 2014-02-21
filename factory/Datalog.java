005da9dd65626455dec700490151a71d9e26b698
Anon <anon@example.com>
2014-02-21 21:07:05 +0100

    New datalog

--- factory/Datalog.java
+++ factory/Datalog.java
@@ -1,4 +1,49 @@
 package labb6.factory;
 
 public class Datalog extends Human {
+
+    private int year;
+
+    public Datalog(int age, String name, int year) {
+        super(age, name);
+        if (year < 1932) {
+            this.year = 1932;
+        } else if (year > 2013) {
+            this.year = 2013;
+        } else {
+            this.year = year;
+        }
+        if (age < 15 + 2013 - year) {
+            setAge(15 + 2013 - year);
+        }
+    }
+
+    public Datalog(String name, int age, int year) {
+        this(age, name, year);
+    }
+
+    public Datalog() {
+        year = (int) (Math.random() * (2013 - 1932) + 1932);
+        if (getAge() < 15 + 2013 - year) {
+            setAge((int) (Math.random() * (100 - (15 + 2013 - year))
+                    + 15 + 2013 - year));
+        }
+    }
+
+    public String toString() {
+        return String.format("%s, började data F%s", super.toString(),
+                String.valueOf(year).substring(2, 4));
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int compareTo(Fysiker fys) {
+        if (getAge() == fys.getAge()) {
+            return year - fys.getYear();
+        } else {
+            return getAge() - fys.getAge();
+        }
+    }
 }
